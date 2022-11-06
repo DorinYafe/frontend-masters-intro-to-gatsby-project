3b223580ff37f9310db0a77087b0361e1d531182
Dorin Yafe <dorin.yafe@abra-it.com>
2022-11-06 14:16:14 +0200

    graphql

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports ={
+    siteMetadata: {
+        siteUrl: 'https://www.dorin.yafe',
+        title: 'Frontend Masters Intro to Gatsby',
+        description: 'Frontend Masters Intro to Gatsby Course',
+    }
+};
